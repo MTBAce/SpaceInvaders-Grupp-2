41b7f9b441f6e10bdc3a7315dfbd88e6e506fcb0
Gustav-t <154264228+Gustav-t@users.noreply.github.com>
2024-10-17 12:18:15 +0200

    Added shooting changes with powerups

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -11,8 +13,9 @@ public class Player : MonoBehaviour
     public ScreenShake screenShake;
 
     Laser laser;
+    
 
-    public float laserCoolDown = 0.36f;
+    float laserCoolDown = 0.36f;
     float timeSinceShot = 0f;
     float speed = 8f;
 
@@ -39,10 +42,7 @@ public class Player : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space) && Time.time >= timeSinceShot + laserCoolDown)
         {
-            timeSinceShot = Time.time;
-            laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
-            screenShake.TriggerShake(0.1f, 0.07f);
-
+            Shoot();
         }
     }
 
@@ -52,5 +52,55 @@ public class Player : MonoBehaviour
         {
             GameManager.Instance.OnPlayerKilled(this);
         }
+
+        if (collision.gameObject.CompareTag("Powerup"))
+        {
+            if (collision.gameObject.name == "Powerup1")
+            {
+                float cooldown = laserCoolDown;
+                laserCoolDown *= 0.5f;
+                StartCoroutine(ShootCoroutine(3, cooldown));
+            } 
+            else if (collision.gameObject.name == "Powerup2")
+            {
+                float cooldown = laserCoolDown;
+                laserCoolDown *= 0.5f;
+                Debug.Log(laserCoolDown);
+                StartCoroutine(ShootCoroutine(3, cooldown));
+            }
+            else
+            {
+                float cooldown = laserCoolDown;
+                laserCoolDown *= 0.5f;
+                Debug.Log(laserCoolDown);
+                StartCoroutine(ShootCoroutine(3, cooldown));
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        timeSinceShot = Time.time;
+        laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+        screenShake.TriggerShake(0.1f, 0.07f);
+    }
+
+    private void Powerup1()
+    {
+
+    }
+    private void Powerup2()
+    {
+
+    }
+    private void Powerup3()
+    {
+
+    }
+
+    IEnumerator ShootCoroutine(float seconds,float cooldown)
+    {
+        yield return new WaitForSeconds(seconds);
+        laserCoolDown = cooldown;
     }
 }
