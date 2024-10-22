c2a26b79332142391e34dc85949bf1d884a90c4a
Sophiyax <sophiya.dhungana@elev.ga.lbs.se>
2024-10-22 19:02:03 +0200

    Playagainbutton

---
+++ Assets/Script/GameManager2.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager2 : MonoBehaviour
+{
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        Time.timeScale = 1;
+
+    }
+
+}
